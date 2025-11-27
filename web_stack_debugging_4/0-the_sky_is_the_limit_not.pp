33ff9ec1121c25d8f19aaaa9995f9478fc60e5bd
deine-mukabucyana <d.mukabucya@alustudent.com>
2025-11-27 15:23:25 +0000

    Fix nginx restart command

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
-# Fix Nginx to handle high load by increasing file descriptor limits
+# Increase the ULIMIT of the default file for Nginx
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
 }
 
 -> exec { 'nginx-restart':
-  command => 'nginx -s reload',
-  path    => '/usr/local/bin/:/usr/bin/:/usr/sbin/:/bin/'
+  command => 'service nginx restart',
+  path    => '/usr/bin/:/usr/sbin/:/bin/',
 }
